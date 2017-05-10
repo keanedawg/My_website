0422cc21dd3451b6676a368a2d90d9abfb93e634
keanedawg <cfife@chicagoventure.com>
2017-05-10 17:25:41 -0500

    Fiddled with some Post model attributes

--- app/models/post.js
+++ app/models/post.js
@@ -1,9 +1,13 @@
 import DS from 'ember-data';
 
+// include later
+// date: DS.attr('date', { defaultValue() { return new Date(); }}),
+
 export default DS.Model.extend({
   title: DS.attr('string'),
+  body: DS.attr('string'),
   content: DS.attr('string'),
   key: DS.attr('string'),
-  date: DS.attr('date', { defaultValue() { return new Date(); }}),
+  timestamp: DS.attr('number'),
   isLocked: DS.attr('boolean')
 });
