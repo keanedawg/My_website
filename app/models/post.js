47c85f20b5a941e47b7270ca1a8a6c040375c05c
keanedawg <cfife@chicagoventure.com>
2017-05-10 16:03:55 -0500

    Added post attributes

--- app/models/post.js
+++ app/models/post.js
@@ -1,5 +1,9 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  title: DS.attr('string'),
+  content: DS.attr('string'),
+  key: DS.attr('string'),
+  date: DS.attr('date', { defaultValue() { return new Date(); }}),
+  isLocked: DS.attr('boolean')
 });
