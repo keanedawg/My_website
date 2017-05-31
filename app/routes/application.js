2d18a954b5bf8f89ca9e3a3bf50376d37153888d
keanedawg <cfife@chicagoventure.com>
2017-05-31 15:42:17 -0500

    made login part of the application template

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,5 +3,22 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     beforeModel: function() {
         return this.get('session').fetch().catch(function() {});
+    },
+    actions: {
+        signIn: function(username, password) {
+            // Essentially the function that makes you "login"
+            this.get('session').open('firebase', 
+                // Provider - the type of authentication (twitter, facebook, github, etc.)
+                // Picking "password" as provider means authenticating with username and password.
+                { provider: "password", 
+                  email: username,
+                  password: password 
+                }).then(function(data) {
+                    console.log(data.currentUser);  // Log information for debugging
+            });
+        },
+        signOut: function() {
+            this.get('session').close();
+        }
     }
 });
