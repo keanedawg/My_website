fe6b341dc51b28acb0795b039a575ba8adac8015
keanedawg <cfife@chicagoventure.com>
2017-06-02 17:17:24 -0500

    created append-html helper

--- app/controllers/write.js
+++ app/controllers/write.js
@@ -5,41 +5,18 @@ export default Ember.Controller.extend({
     publishPost: function() {
       var newPost = this.store.createRecord('post', {
         title: this.get('title'),
-        body: this.get('body'),
+        body: this.get('myProperty'),
         date: new Date().getTime()
       });
       newPost.save();
     },
     setAndSave: function(html) {
-            // Triggers save every 3000 ms as long as user is changing post
-            // Amount of ms can be altered below
+      // Triggers save every 3000 ms as long as user is changing post
+      // Amount of ms can be altered below
 
-            console.log(html); // Contains the HTML from the editor
-
-            let thisModel = this.get('model');
-            console.log(thisModel);
-            let id = thisModel.id;
-            this.store.findRecord('post', id).then(function (post) {
-                let thisId = post.get('id');
-                let thisTitle = post.get('title');
-                let thisContent = post.get('content');
-                console.log(thisId, thisTitle, thisContent);
-                post.set('content', html);
-                post.save();
-            });
-            this.store.findRecord('post', id).then(function (post) {
-                let thisContent2 = post.get('content');
-                console.log(thisContent2);
-            });
-            let thisModel2 = this.get('model');
-            console.log(thisModel2);
-
-            this.set('model.someProp', html);
-            Ember.run.debounce(
-                this,
-                this.getModelSave,
-                3000
-            );
-        },
+      console.log(html); // Contains the HTML from the editor
+      var content = this.get('model.body')
+      console.log(content);
+    }
   }
 });
---
+++ tests/unit/helpers/append-html-test.js
@@ -0,0 +1,12 @@
+
+import { appendHtml } from 'cams-website/helpers/append-html';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | append html');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = appendHtml([42]);
+  assert.ok(result);
+});
+
